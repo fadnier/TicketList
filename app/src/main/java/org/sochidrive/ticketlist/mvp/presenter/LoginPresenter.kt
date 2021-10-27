b12b866d928a4c431a0ca24c3ce044df397abd05
fadnier <fadnier@sochidrive.org>
2021-10-27 22:10:01 +0300

    homework2-test2

--- app/src/main/java/org/sochidrive/ticketlist/mvp/presenter/LoginPresenter.kt
+++ app/src/main/java/org/sochidrive/ticketlist/mvp/presenter/LoginPresenter.kt
@@ -2,6 +2,7 @@ package org.sochidrive.ticketlist.mvp.presenter
 
 import io.reactivex.rxjava3.core.Scheduler
 import moxy.MvpPresenter
+import org.sochidrive.ticketlist.mvp.model.entity.AuthAnswer
 import org.sochidrive.ticketlist.mvp.model.helpdesk.IHelpdeskLogin
 import org.sochidrive.ticketlist.mvp.view.LoginView
 import org.sochidrive.ticketlist.navigation.Screens
@@ -21,31 +22,30 @@ class LoginPresenter: MvpPresenter<LoginView>() {
         super.onFirstViewAttach()
         viewState.init()
         viewState.loadLogo("https://api.tvintel.info/pic/logo_tvintel3.png")
-        authManager.checkCacheAuth().observeOn(mainThreadScheduler).subscribe({
-            if(it.result=="Ok") {
-                viewState.showMessage("Востановление: "+it.data.name)
-                router.replaceScreen(Screens.MainMenuScreen(it.data))
-            } else {
-                //viewState.showMessage(it.data.answer)
-            }
-        },{
-            it.fillInStackTrace()
-        })
+//        authManager.checkCacheAuth().observeOn(mainThreadScheduler).subscribe({
+//            auth(it)
+//        },{
+//            it.fillInStackTrace()
+//        })
     }
 
     fun clickLoginBtn(login: String, password: String) {
         authManager.getAuth(login,password)
                 .observeOn(mainThreadScheduler)
                 .subscribe({
-                    if(it.result=="Ok") {
-                        viewState.showMessage("Успешно: "+it.data.name)
-                        router.replaceScreen(Screens.MainMenuScreen(it.data))
-                    } else {
-                        viewState.showMessage(it.data.answer)
-                    }
+                    auth(it)
                 },{
                     it.fillInStackTrace()
                 })
+    }
+
+    fun auth(authAnswer: AuthAnswer) {
+        if(authAnswer.result=="Ok") {
+            viewState.showMessage("Успешно: "+authAnswer.data.name)
+            router.replaceScreen(Screens.MainMenuScreen(authAnswer.data))
+        } else {
+            viewState.showMessage(authAnswer.data.answer)
+        }
     }
 
     fun backClick(): Boolean {
